06928735001040aaac119e90b81d1e2edf8d1694
Celestial-Entity <139852159+stardoom4@users.noreply.github.com>
2024-03-13 00:32:45 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,25 @@
 const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
+  // Add the eleventySass plugin
   eleventyConfig.addPlugin(eleventySass);
 
+  // Parse Markdown files without front matter
+  eleventyConfig.setFrontMatterParsingOptions({
+    engines: {
+      markdown: require('markdown-it')({
+        html: true,
+        breaks: true,
+        linkify: true
+      })
+    },
+    options: {
+      // Exclude front matter from output
+      frontmatter: false,
+    },
+  });
+
+  // Configure Eleventy settings
   return {
     dir: {
       input: "src",
